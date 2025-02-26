a63dd96e9e8082018d314bf9d83b4ac9a971fd35
Terrence Clayton <184171442+quarrybreeze@users.noreply.github.com>
2025-02-25 22:53:25 -0800

    fixed a bug where empty buckets = nil would cause crash. added logic to ensure != nil

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -10,7 +10,7 @@ class HashMap
     @buckets = Array.new(capacity) {[]}
   end
 
-  def hash(key)
+  def hash(key) #provided by project
     hash_code = 0
     prime_number = 31
        
@@ -20,20 +20,35 @@ class HashMap
   end
 
   def set(key, value)
+
     hash_code = self.hash(key)
-    hash = hash_code % @capacity #temporary
+    hash = hash_code % @capacity
     data = [key,value]
 
     if @buckets[hash][0] == nil                     #if no head
       @buckets[hash][0] = Node.new(data, nil)
-    elsif @buckets[hash][0].value.first == key
+    elsif @buckets[hash][0].value.first == key  #if head, check the first key
       @buckets[hash][0].value = data
     else
-      @buckets[hash][0].next_node = Node.new(data,nil)
+      @buckets[hash][0].next_node = Node.new(data,nil) #could probably use improvement to make longer collisions
       # p "Collision detected #{key} and #{value}"
+    end
+    
+    if self.length > (@load_factor.to_f*@capacity.to_i)
+      self.resize
     end
 
   end
+
+  def resize
+    @capacity *= 2
+    entries_array = self.entries 
+    self.clear           
+    entries_array.each do |entry| 
+      self.set(entry[0], entry[1])
+    end
+  end
+  
 
   def get(key)
     hash_code = self.hash(key)
@@ -41,7 +56,7 @@ class HashMap
 
     current = @buckets[hash][0]
     
-    if self.has?(key) == false
+    if self.has?(key) == false   #if key exists, jump to next node and find key and then return the value
       return nil
     else
       while current.value.first != key
@@ -49,31 +64,9 @@ class HashMap
       end
       current.value.last
     end
-    # if @buckets[hash][0] == nil
-    #   return nil
-    # elsif @buckets[hash][0].value.first == key
-    #   return @buckets[hash][0].value.last
-    # else
-    #   while @buckets[hash][0].next_node != nil
-    #     @buckets[hash][0] = @buckets[hash][0].next_node
-    #     if @buckets[hash][0].value.first == key
-    #       return @buckets[hash][0].value.last
-    #     end
-    #   end
-    # end
+
   end
 
-  # def node_length(key)
-  #   hash_code = self.hash(key)
-  #   hash = hash_code % @capacity
-  #   length = 1
-
-  #   while @buckets[hash][0].next_node != nil
-  #     length += 1
-  #     @buckets[hash][0] = @buckets[hash][0].next_node
-  #   end
-  #   length
-  # end
 
   def has?(key)
     hash_code = self.hash(key)
@@ -89,7 +82,7 @@ class HashMap
     else
       while current.next_node != nil
         current = current.next_node
-        if current.value.first == key
+        if current.value.first == key 
           found = true
         end
       end
@@ -129,61 +122,63 @@ class HashMap
   end
 
   def clear
-
+    @buckets = Array.new(@capacity) {[]}
   end
+  
 
   def keys
-    
+    keys_array = []
+
+    for i in 0..(@capacity-1)
+      if @buckets[i] != nil && @buckets[i][0] != nil
+        keys_array << @buckets[i][0].value.first
+        current = @buckets[i][0]
+        while current.next_node != nil
+          current = current.next_node
+          keys_array << current.value.first
+        end
+      end
+    end
+    return keys_array
   end
 
   def values
+    values_array = []
 
+    for i in 0..(@capacity-1)
+      if @buckets[i] != nil && @buckets[i][0] != nil
+        values_array << @buckets[i][0].value.last
+        current = @buckets[i][0]
+        while current.next_node != nil
+          current = current.next_node
+          values_array << current.value.last
+        end
+      end
+    end
+    return values_array
+  end
+
+  def entries
+    entries_array = []
+    individual_entry = []
+
+    for i in 0..(@capacity-1)
+      if @buckets[i] != nil && @buckets[i][0] != nil  
+        individual_entry << @buckets[i][0].value.first
+        individual_entry << @buckets[i][0].value.last
+        entries_array << individual_entry
+        individual_entry = []
+        current = @buckets[i][0]
+        while current.next_node != nil
+          current = current.next_node
+          individual_entry << current.value.first
+          individual_entry << current.value.last
+          entries_array << individual_entry
+          individual_entry = []
+        end
+      end
+    end
+    return entries_array
   end
 
 end
-
-test = HashMap.new
-
-test.set('apple', 'red')
-test.set('banana', 'yellow')
-test.set('carrot', 'orange')
-test.set('dog', 'brown')
-test.set('elephant', 'gray')
-test.set('frog', 'green')
-test.set('grape', 'purple')
-test.set('hat', 'black')
-test.set('ice cream', 'white')
-test.set('jacket', 'blue')
-test.set('kite', 'pink')
-test.set('lion', 'golden')
-p test.length
-
-# p test.has?('grape')
-# p test.get('grape')
-# p test.has?('hat')
-# p test.get('hat')
-# p test.remove('grape')
-# p test.get('grape')
-# p test.has?('grape')
-# p test.has?('hat')
-# p test.get('hat')
-# p test.remove('hat')
-# p test.has?('hat')
-# p test.get('grape')
-# p test.get('hat')
-
-# p test.node_length('hat')
-
-
-# p test.hash('apple') #10
-# p test.hash('banana') #5
-# p test.hash('carrot') #3
-# p test.hash('dog') #12
-# p test.hash('elephant') #1
-# p test.hash('frog') #4
-# p test.hash('grape') #11
-# p test.hash('hat') #11
-# p test.hash('ice cream') #13
-# p test.hash('jacket') #14
-# p test.hash('kite') #15
-# p test.hash('lion') #12
--- main.rb
+++ main.rb
@@ -1,1 +1,49 @@
 require_relative 'lib/hashmap'
+
+test = HashMap.new
+
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+
+p test.length
+
+p test.capacity
+
+test.set('elephant', 'turqoise')
+test.set('frog', 'lime')
+test.set('grape', 'butterscotch')
+
+p test.length
+
+p test.capacity
+
+test.set('moon', 'silver')
+
+p test.length
+p test.capacity
+
+test.set('jacket', 'rainbow')
+test.set('kite', 'sky blue')
+test.set('lion', 'slate')
+
+p test.length
+
+p test.get('elephant')
+p test.has?('elephant')
+p test.keys
+p test.values
+p test.entries
+test.clear
+p test.entries
+
+p test.capacity
